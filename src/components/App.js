2d0075d51818ee1c4975880bcf32f201b193b335
Serhii Vandenvin <sevandenvin@gmail.com>
2022-11-11 16:44:27 +0200

    test

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,14 @@
-import { Profile } from './Profile/Profile';
-import user from '../user.json';
-import { Statistics } from './Statistics/Statistics';
-import data from '../data.json';
-import css from './App.module.css';
+import { Profile } from "./Profile/Profile";
+import user from "../user.json";
+import { Statistics } from "./Statistics/Statistics";
+import data from "../data.json";
+import css from "./App.module.css";
 
 export const App = () => {
   return (
     <>
       <div className={css.container}>
-        <h1>Task 1: Social network profile</h1>
+        <h1>Task 1: Social network profile te te te</h1>
         <Profile
           username={user.username}
           tag={user.tag}
